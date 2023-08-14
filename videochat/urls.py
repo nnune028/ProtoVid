44336d67db03ad3230032202e8a7b3ba6abbd51c
Nicholas Nunez <nnune028@fiu.edu>
2023-08-14 19:44:24 -0400

    Add URLs as possible fix for deployment issues.

--- videochat/urls.py
+++ videochat/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings # Allows us to connect to MEDIA_ROOT/MEDIA_URL
+from django.conf.urls.static import static # Allows us to create a URL for our static files
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) # For access to staticfiles
